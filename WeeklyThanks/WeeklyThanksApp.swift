8a795391562b9004903635963238f7f7dc967f94
Carl Rudling <carl@rudling.eu>
2024-02-23 21:21:15 +0100

    Initial Commit

---
+++ WeeklyThanks/WeeklyThanksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyThanksApp.swift
+//  WeeklyThanks
+//
+//  Created by Carl Rudling on 2024-02-23.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyThanksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
