8306b60cf5001654055e36b5f5c41595d433269d
Carl Rudling <carl@rudling.eu>
2024-03-31 20:28:04 +0200

    Ready for first Launch

---
+++ WeeklyThanks/Views/Start/ExplanationView.swift
@@ -0,0 +1,18 @@
+//
+//  ExplanationView.swift
+//  WeeklyThanks
+//
+//  Created by Carl Rudling on 2024-03-31.
+//
+
+import SwiftUI
+
+struct ExplanationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExplanationView()
+}
