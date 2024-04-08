3f858611995c28acd8e650b8e60f1d69bce8c028
Carl Rudling <carl@rudling.eu>
2024-04-08 23:56:18 +0200

    Started to create the foundation for different themes

---
+++ WeeklyThanks/Views/ChangeCardThemeView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeCardThemeView.swift
+//  WeeklyThanks
+//
+//  Created by Carl Rudling on 2024-04-08.
+//
+
+import SwiftUI
+
+struct ChangeCardThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeCardThemeView()
+}
