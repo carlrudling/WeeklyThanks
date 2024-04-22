5f56aa53ef1b81b720c6d18045b8387e623f03b4
Carl Rudling <carl@rudling.eu>
2024-04-22 23:14:03 +0200

    Change how card designs are choosen

---
+++ WeeklyThanks/Views/ChooseDesignView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseDesignView.swift
+//  WeeklyThanks
+//
+//  Created by Carl Rudling on 2024-04-22.
+//
+
+import SwiftUI
+
+struct ChooseDesignView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseDesignView()
+}
